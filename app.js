e6c92c14aea3daf71794240d3403fb654388d770
Andres Jose Gutierrez Marin <anjgutie@bancolombia.com.co>
2024-02-05 11:53:32 -0500

    Routes and middleware usage

--- app.js
+++ app.js
@@ -4,18 +4,47 @@ const fs = require('fs');
 const app = express();
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log('Hello from the middleware ✌');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 
-app.get('/api/v1/tours', (req, res) => {
+
+
+const getAllTours = (req, res) => {
+    console.log(req.requestTime);
     res
         .status(200)
         .json({
             status: 'success',
             data: { tours }
         });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTourById = (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(t => t.id === id);
+    var statusCode = 200;
+    var status = 'success';
+    if (!tour) {
+        statusCode = 404;
+        status = 'not found'
+    }
+    res
+        .status(statusCode)
+        .json({
+            status: status,
+            data: { tour }
+        });
+};
+
+const createTour = (req, res) => {
     const { body } = req;
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, body);
@@ -28,7 +57,40 @@ app.post('/api/v1/tours', (req, res) => {
         });
     })
 
-});
+};
+
+const deleteTourById = (req, res) => {
+    const id = req.params.id * 1;
+    const newTours = tours.filter(tour => tour.id !== id);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(newTours), err => {
+        if (err) {
+            res.status(500).json({
+                status: 'failure',
+                data: null
+            })
+            console.log('Could not delete de tour from database');
+        }
+        else {
+            res.status(200).json({
+                status: 'success',
+                data: null
+            })
+        }
+    })
+};
+
+
+
+app
+    .route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour)
+
+app
+    .route('/api/v1/tours/:id')
+    .get(getTourById)
+    .delete(deleteTourById)
+
 
 const port = 8080;
 app.listen(port, () => {

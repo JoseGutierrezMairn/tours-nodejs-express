3eff67d82c35e0634fa3ccacbf86634947de5802
Andres Jose Gutierrez Marin <anjgutie@bancolombia.com.co>
2024-02-08 16:16:37 -0500

    Creating a schema and a model with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 8080;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB.replace(/<PASSWORD>/g, process.env.DB_PASS);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('DB Connection Successful ✔'));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    defaul: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App is running on port ${port}...`);
+  console.log(`App is running on port ${port}...`);
 });
